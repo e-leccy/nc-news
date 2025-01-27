62310c7f1a937df3d1a54f1700b0d8e280b78048
e-leccy <eleccy@aim.com>
2025-01-27 14:23:23 +0000

    add missing code to handle server (500) errors

--- db/errors.js
+++ db/errors.js
@@ -1,4 +1,4 @@
-exports.handleServerErrors = (error, request, response, next) => {
-  console.log(error);
+exports.handleServerErrors = (err, request, response, next) => {
+  console.log(err);
   response.status(500).send({ error: "Internal Server Error" });
 };
